d36f953659fd49b691f4b0acd9b6f66a517e9337
CCS <ccs@crestonecharterschool.fake>
2020-01-30 14:36:10 -0700

    First commit

---
+++ template-parts/carousel.php
@@ -0,0 +1,38 @@
+
+<div class="carousel-wrapper">
+  <div class="carousel">
+
+<?php
+
+$category_id = get_category_by_slug('carousel')->cat_ID;
+
+$query_images_args = array(
+    'post_type'         => 'attachment',
+    'post_mime_type'    => 'image',
+    'post_status'       => 'inherit',
+    'posts_per_page'    => -1,
+    'cat'               => $category_id,
+);
+
+$query_image = new WP_Query($query_images_args);
+
+foreach ($query_image->posts as $index => $image) {
+
+?> 
+
+<img class="carousel__photo <?php
+  if ( $index === 0 ) { ?> initial <?php } ?>" 
+           src="<?php echo wp_get_attachment_url( $image->ID ); ?>">
+
+<?php 
+} 
+?>
+
+        <div class="carousel__button--next"></div>
+        <div class="carousel__button--prev"></div>
+    </div>
+</div>
+
+<?php
+wp_reset_query();
+?>
